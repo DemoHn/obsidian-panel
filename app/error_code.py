8ec69b7dec1534f88787fe0ccc2875d39bad8844
DemoHn <demohn@foxmail.com>
2016-08-03 15:59:30 +0800

    fetch some legacy code (utils.py)

---
+++ app/error_code.py
@@ -0,0 +1,90 @@
+__author__ = 'Nigshoxiz'
+# date : 2015-11-17
+# errcode
+
+errcode = {}
+
+errcode['300'] = "outdated"
+
+# refuse op
+errcode['340'] = "refuse operation"
+# sock
+errcode['400'] = "json parsing error"
+errcode['401'] = "input data is not complete"
+errcode['402'] = "operation is not allowed"
+errcode['403'] = "client IP not Host!"
+errcode['405'] = "invalid keyword"
+# new_service dispatch error
+errcode['420'] = "dispatch unknown error"
+errcode['421'] = "dispatch out of quota"
+errcode['422'] = "can't dispatch a port"
+errcode['423'] = "create instance error"
+errcode['424'] = "process creation error"
+
+# service_idf error
+errcode['430'] = "service_idf not available"
+errcode['431'] = "device num is full"
+errcode['432'] = "identifier collision!!"
+# process error
+errcode['450'] = "kill process error"
+errcode["451"] = "create process error"
+
+# when triggering try...except...
+errcode["500"] = "fatal error"
+errcode['520'] = "data is null"
+
+# database
+errcode["600"] = "database error"
+errcode["610"] = "can't fetch data"
+errcode["620"] = "data is null"
+errcode["621"] = "ssid does not exist"
+errcode["631"] = "service name not unique"
+errcode["700"] = "illegal command"
+
+# server_list change_remains error
+errcode["710"] = "decline too much"
+errcode["720"] = "lid not active"
+errcode["730"] = "quota setting error"
+errcode["740"] = "register server error"
+# login
+errcode["730"] = "username duplicated"
+errcode["731"] = "no such account"
+
+#register error
+errcode["800"] = "null username is illegal"
+errcode["801"] = "password should not shorter than six characters"
+
+errcode["810"] = "unknown error"
+errcode["811"] = "no such username"
+errcode["812"] = "password error"
+
+errcode["830"] = "auth error"
+
+# check purchase string
+errcode["840"] = "purchase check error"
+errcode["841"] = "no service to purchase"
+
+# register new sub-server
+errcode["860"] = "server key error"
+
+errcode["880"] = "HTTP communication error"
+errcode["881"] = "socket timeout"
+errcode["882"] = "HTTP execute error"
+# bind error
+errcode["900"] = "bind account error"
+errcode["901"] = "router IDF not unique"
+
+# wallet
+errcode["1000"] = "balance not enough"
+errcode["1001"] = "recharge amount is larger than upper limit"
+# pricing strategy
+errcode["1010"] = "strategy name error"
+
+#dispatch
+errcode["1020"] = "no server to dispatch"
+errcode["1021"] = "dispatch fails"
+#etc
+errcode["1120"] = "quota adjust error"
+
+# free service
+errcode["1200"] = "already accepted a free service"
--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,251 @@
-# OB panel utility functions and classes.
-# by DemoHn
-# 2016.7.31
+#coding=utf-8
+__author__ = 'Nigshoxiz'
 
-class utils:
-    @staticmethod
-    def hello():
+import time
+import datetime
+import re, os
+import json
+import calendar
+from app.error_code import errcode
+import pytz
+
+def get_file_directory():
+    full_path = os.path.realpath(__file__)
+    path,file = os.path.split(full_path)
+    return path
+
+class timeUtil:
+    def __init__(self):
         pass
+
+    @staticmethod
+    def getCurrentUTCtimestamp():
+        # get timestamp
+        # time.timezone represents for the seconds that current timestamp delayed
+        return int(datetime.datetime.now().timestamp())
+
+    @staticmethod
+    def getReadableTime(utc_timestamp,timezone):
+        # final string e.g: 2015-10-12 17:39
+        local_timestamp = int(utc_timestamp) + int(timezone)*3600
+        return datetime.datetime.utcfromtimestamp(local_timestamp).strftime("%Y-%m-%d %H:%M:%S")
+
+    # def getUTCtimestamp():
+    # INPUT :
+    @staticmethod
+    def getUTCtimestamp(readable_time,timezone):
+        # here, timezone means hours before UTC
+        # e.g: CST <--> +8
+        re_str = "([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)"
+        m = re.search(re_str,readable_time)
+
+        if m == None:
+            return False
+        else:
+            year   = m.group(1)
+            month  = m.group(2)
+            date   = m.group(3)
+            hour   = m.group(4)
+            minute = m.group(5)
+
+            # first, we assume that the input datestring is a UTC datetime
+            dm     = datetime.datetime(int(year),int(month),int(date),int(hour),int(minute))
+            tm     = calendar.timegm(dm.timetuple())
+            return int(tm) - timezone * 3600
+
+class dateUtil:
+
+    # get current LOCAL time
+    @staticmethod
+    def now():
+        return datetime.datetime.now()
+
+    @staticmethod
+    def getTimestamp(year,month,day):
+        n_dm     = datetime.datetime(int(year),int(month),int(day),0,0,0)
+        tm     = calendar.timegm(n_dm.timetuple())
+        return int(tm)
+
+    @staticmethod
+    def getCurrentDatetime():
+        dm = datetime.datetime.utcnow()
+        year  = int(dm.strftime("%Y"))
+        month = int(dm.strftime("%m"))
+        day   = int(dm.strftime("%d"))
+        hour  = int(dm.strftime("%H"))
+        minute= int(dm.strftime("%M"))
+        second= int(dm.strftime("%S"))
+
+        date = {}
+        date["year"] = year
+        date["month"] = month
+        date["date"] = day
+        date["hour"] = hour
+        date["minute"] = minute
+        date["second"] = second
+        return date
+
+    @staticmethod
+    def getCurrentTimeRemainRatioOfMonth():
+        dm = datetime.datetime.utcnow()
+        year  = int(dm.strftime("%Y"))
+        month = int(dm.strftime("%m"))
+
+        last_day     = calendar.monthrange(year,month)[1]
+        first_day_tp = datetime.datetime(year,month,1,0,0,0)
+        last_day_tp  = datetime.datetime(year,month,last_day,23,59,59)
+
+        # calc timestamp
+        now_stamp    = float(time.time() + time.timezone) # UTC timestamp
+        first_stamp  = float(time.mktime(first_day_tp.timetuple()))
+        last_stamp   = float(time.mktime(last_day_tp.timetuple()))
+
+        return (last_stamp - now_stamp) / (last_stamp - first_stamp)
+
+    @staticmethod
+    def DatetimeToTimestamp(date_time):
+        return int(time.mktime(date_time.timetuple()))
+
+    @staticmethod
+    def getDateAfterDays(relative_days):
+        rd = int(relative_days)
+
+        dm = datetime.datetime.utcnow()
+        year  = int(dm.strftime("%Y"))
+        month = int(dm.strftime("%m"))
+        day   = int(dm.strftime("%d"))
+
+        n_dm     = datetime.datetime(int(year),int(month),day,0,0,0)
+        tm     = time.mktime(n_dm.timetuple())-time.timezone+rd*24*3600
+
+        tms = datetime.datetime.utcfromtimestamp(tm).strftime("%Y-%m-%d %H:%M:%S")
+
+        re_str = "([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)"
+        m = re.search(re_str,tms)
+
+        return (m.group(1) , m.group(2), m.group(3))
+
+    @staticmethod
+    def getDateAfterMonths(relative_months,year=0,month=0,day=0):
+
+        def _is_leap_year(year):
+            if year % 4 == 0:
+                if year % 100 == 0:
+                    if year % 400 == 0:
+                        return 1
+                    else:
+                        return 0
+                else:
+                    return 1
+            else:
+                return 0
+
+        months = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+        rm = int(relative_months)
+
+        dm = datetime.datetime.utcnow()
+        if year == 0:
+            year  = int(dm.strftime("%Y"))
+        if month == 0:
+            month = int(dm.strftime("%m"))
+        if day == 0:
+            day   = int(dm.strftime("%d"))
+        elif day == "last":
+            if month == 2:
+                day = 28 + _is_leap_year(year)
+            else:
+                day = months[month]
+
+        year += int(rm / 12)
+        rm = rm % 12
+
+        month = month + rm
+        if rm + month > 12:
+            year += 1
+            month -= 12
+
+        days_of_feb = 28 + _is_leap_year(int(year))
+        if month != 2 and day > months[month]:
+            day = months[month]
+        elif month == 2 and day > days_of_feb:
+            # last day of Feb
+            day = days_of_feb
+
+        return (year,month,day)
+        pass
+
+class returnModel:
+    def __init__(self,type="json"):
+        self.rtn_type = type
+        pass
+
+    def success(self,info,code=200,type="json"):
+        rtn = {
+            "status":"success",
+            "code" :code,
+            "info" : info
+        }
+
+        if self.rtn_type == "string":
+            return json.dumps(rtn)
+        else:
+            return rtn
+
+    def error(self,error_code,info="",type="json"):
+        rtn = {
+            "status":"error",
+            "code" :error_code,
+            "info" : ""
+        }
+
+        if info != "":
+            rtn["info"] = info
+        else:
+            try:
+                rtn["info"] = errcode[str(error_code)]
+            except Exception as e:
+                rtn["info"] = "unknown error description"
+
+        if self.rtn_type == "string":
+            return json.dumps(rtn)
+        else:
+            return rtn
+
+class testUtil:
+    def compare(item,return_info,code=0,info=""):
+        pass
+        # standard return_info:
+        # {
+        #    "status" : "success" | "error",
+        #    "code" : XXX,
+        #    "info" : YYY
+        # }
+
+# 统计本目录下所有*.py文件加在一起的总行数
+def get_line_number(directory):
+    num = 0
+    # get file list
+    file_list = os.listdir(directory)
+
+    for item in file_list:
+        _item = item
+        item = os.path.normpath(directory+"/"+item)
+        # if it is file and it is *.py
+        if os.path.isfile(item) and item.find(".py") > 0 and item.find(".pyc") < 0 \
+                and item.find("bottle.py") < 0:
+            f = open(item,"rb")
+            nums = len(f.readlines())
+            num += nums
+            print("---"+str(_item)+": "+str(nums))
+            f.close()
+        elif os.path.isdir(item+"/") and item != ".git" and item != "assets" and item != "lib":
+            subdir = os.path.normpath(item)
+
+            num += get_line_number(subdir)
+    return num
+# line number stat
+#print("\nfinal line number: "+str(get_line_number(get_file_directory())))
+
+# test f**king dateUtil
+#print(timeUtil.getReadableTime(timeUtil.getCurrentUTCtimestamp(),8))
+#print(timeUtil.getUTCtimestamp("2016-6-23 00:25",0))
