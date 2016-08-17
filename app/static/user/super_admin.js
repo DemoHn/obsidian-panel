63f707017a36da478e2bb6eac7584ffe6791fda8
DemoHn <demohn@foxmail.com>
2016-08-18 00:32:41 +0800

    add upload-core-file subpage

--- app/static/user/super_admin.js
+++ app/static/user/super_admin.js
@@ -2,16 +2,50 @@
 Vue.config.delimiters = ['${','}'];
 
 $(document).ready(function(){
-    // tooltip initialization
-    //$("#_logout").tooltip();
-    var socket = io.connect("/dw");
-    console.log("connect:")
-    socket.on("connect", function () {
-        console.log("f")
-        socket.emit("hw",{'a':'b'})
-    })
+    var pathname = location.pathname;
+    var path_arr = pathname.split("/");
+    var path_item = path_arr[path_arr.length - 1];
 
-    socket.on("oh",function (msg) {
-        console.log(msg);
-    })
+    var _map = {
+        'server_core' : ServerCorePage
+    };
+
+    // init
+    _map[path_item]();
 });
+
+/*
+* Server Core Page Management
+* */
+var ServerCorePage = function () {
+    this.upload_url = "/super_admin/upload_server_core";
+    this.upload_vm = new Vue({
+        el:"#upload_core",
+        data:{
+            allow_upload : true,
+            file_name : "",
+            mc_version : "",
+            file_version : "",
+            description : ""
+        },
+        computed:{
+            allow_upload : function () {
+                return !!(this.mc_version.length > 0 && this.file_name != "");
+            }
+        },
+        methods:{
+            "upload_file" : function () {
+                console.log(this.core_type)
+            },
+            "on_file_change" : function (e) {
+                var files = e.target.files || e.dataTransfer.files;
+
+                this.file_name = files[0]["name"];
+            }
+        }
+    });
+};
+
+ServerCorePage.prototype._uploadFile = function (callback) {
+    //$("#_upload_core_file").fileupload()
+};
