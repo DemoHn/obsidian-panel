2cb92742aba23e05933cbf65ab78c69e4f79636b
DemoHn <demohn@foxmail.com>
2016-09-30 22:58:24 +0800

    hey, ftp

--- ftm/manager.py
+++ ftm/manager.py
@@ -4,12 +4,11 @@ from pyftpdlib.authorizers import DummyAuthorizer, AuthenticationFailed
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer, ThreadedFTPServer
 from app.utils import salt
+from app.controller.global_config import GlobalConfig
 
 import threading
-from multiprocessing import Process, Manager
-import socket
 import hashlib
-import os
+import traceback
 
 class MD5Authorizer(DummyAuthorizer):
     def validate_authentication(self, username, password, handler):
@@ -43,9 +42,74 @@ class FTPManager(object):
         self.listening_port = port
 
         self.server_process = None
+        # read global config
+        self._global_config = GlobalConfig.getInstance()
 
-    def add_user(self, username, password, working_dir, permission="elradfmw"):
-        hash = hashlib.md5(password.encode('utf-8') + salt).hexdigest()
+        if self._get_initdb_status():
+            self._add_account_data()
+
+    def _get_initdb_status(self):
+        if self._global_config.get("ob_init_flag") == None:
+            return False
+        else:
+            return True
+
+    def _add_account_data(self):
+        db_type = self._global_config.get("db_type")
+        data = []
+        exec_statement = "SELECT * FROM ob_ftp_account"
+        if self._global_config.get("init_super_admin") == True:
+            db_env = self._global_config
+
+            if db_type == "sqlite":
+                database_uri = "%s/%s.db" % (db_env.get("sqlite_dir"),
+                                                       db_env.get("db_name"))
+                conn = None
+                try:
+                    import sqlite3
+                    conn = sqlite3.connect(database_uri)
+                    cursor = conn.cursor()
+                    cursor.execute(exec_statement)
+
+                    data = cursor.fetchall()
+                except:
+                    traceback.print_exc()
+                finally:
+                    if conn != None:
+                        conn.close()
+            else:
+                conn = None
+                try:
+                    import pymysql
+                    conn = pymysql.connect(host=db_env.get("db_mysql_ip"),
+                             user= db_env.get("db_mysql_username"),
+                             password=db_env.get("db_mysql_password"),
+                             db=db_env.get("db_name"),
+                             charset='utf8mb4')
+
+                    cursor = conn.cursor()
+                    cursor.execute(exec_statement)
+
+                    data = cursor.fetchall()
+                except:
+                    traceback.print_exc()
+                finally:
+                    if conn != None:
+                        conn.close()
+            # connect database server
+            # add user
+            for item in data:
+                _username = item[1]
+                _hash     = item[2]
+                _work_dir = item[3]
+                _permission = item[6]
+                self.add_user(_username, _hash, _work_dir, permission=_permission)
+        else:
+            # relax, there's nothing to do
+            return None
+
+    def add_user(self, username, hash, working_dir, permission="elradfmw"):
+        #hash = hashlib.md5(password.encode('utf-8') + salt).hexdigest()
         self.authorizer.add_user(username, hash, working_dir,
                                  perm = permission,
                                  msg_login=self.login_msg,
--- start_ftp_server.py
+++ start_ftp_server.py
@@ -1,45 +1,5 @@
 __author__ = "Nigshoxiz"
 
-'''
-[circus]
-endpoint = tcp://127.0.0.1:5555
-pubsub_endpoint = tcp://127.0.0.1:5556
-stats_endpoint = tcp://127.0.0.1:5557
-
-[watcher:web]
-copy_env = True
-virtualenv = ./env
-working_dir = ./
-
-use_sockets = True
-cmd = ./env/bin/chaussette run.app
-args = --fd $(circus.sockets.web) --backend eventlet
-numprocesses = 5
-
-[socket:web]
-host = 0.0.0.0
-port = 5000
-'''
-
-# Dev Note:
-# For some strange reasons, it is not allowed to run a chaussette instance
-# using eventlet backend in python3 (chaussette version: v0.13.0, which is the newest version pip could download).
-# That is, we can't run `chaussette run.app --backed eventlet` to start server directly.
-# In fact, chaussette supports eventlet backend in python3 internally, it just denies `eventlet` as an option
-# of `--backend` in command line. Considering the stability of eventlet library in python3, which has been
-# improved a lot since the release date of chaussette v0.13.0 (2015), we monkey-patch it to bypass the
-# restriction of command line .
-#
-# And How to run in circus?
-#
-# Just append the following configuration:
-#
-# [watcher:xxx]
-# cmd = python start_chaussette.py --fd $(circus.sockets.web)
-#
-# Nigshoxiz
-# 2016-8-16
-
 # import app
 from ftm import FTPManager
 from ftm.manager import ServerThread
