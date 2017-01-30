3dd306abd927bd2cc42a38013dada586b05c23de
DemoHn <demohn@foxmail.com>
2017-01-30 16:54:13 +0800

    add `server info` page (CPU, OS, total RAM)

--- app/blueprints/superadmin/info.py
+++ app/blueprints/superadmin/info.py
@@ -3,10 +3,14 @@ __author__ = "Nigshoxiz"
 from flask import render_template, abort, request, make_response, redirect, session
 from jinja2 import TemplateNotFound
 
+import sys, os, distro, platform
 from app import db, app
 from app.controller.global_config import GlobalConfig
 from app.utils import returnModel, get_file_hash
+from app.tools.cpuinfo import cpu
 from app.model import ServerCORE
+
+from psutil import virtual_memory
 
 from . import super_admin_page, logger
 from .check_login import super_admin_only, ajax_super_admin_only
@@ -21,3 +25,41 @@ def render_info_page(uid, priv):
         return render_template('superadmin/index.html')
     except TemplateNotFound:
         abort(404)
+
+@super_admin_page.route('/info/get_server_info', methods=['GET'])
+@super_admin_only
+def get_server_info(uid, priv):
+    _model = {
+        "cpu" : {
+            "vendor" : "",
+            "model" : "",
+            "cores" : "",
+            "freq" : ""
+        },
+        "OS" : {
+            "arch" : "",
+            "kernel" : "",
+            "name" : "",
+            "distro" : ""
+        },
+        "memory" : ""
+    }
+
+    _model["OS"]["name"]   = platform.system()
+    memory_GB = virtual_memory().total /1024/1024/1000.0
+    _model["memory"] = "%.2f" % memory_GB
+
+    # TODO: add windows support
+    if sys.platform.startswith("linux"):
+        _cpu_model = {
+            "vendor" : cpu.info[0].get("vendor_id"),
+            "model" : cpu.info[0].get("model name"),
+            "cores" : len(cpu.info),
+            "freq" : cpu.info[0].get("cpu MHz")
+        }
+
+        _model["cpu"] = _cpu_model
+        _model["OS"]["kernel"] = os.popen("uname -mrs").read()[:-1]
+        _model["OS"]["distro"] = "%s %s %s" % distro.linux_distribution()
+        _model["OS"]["arch"] = platform.machine()
+    return rtn.success(_model)
--- frontend/src/views/super_admin/Info.vue
+++ frontend/src/views/super_admin/Info.vue
@@ -12,22 +12,59 @@
                         </div>
                     </div>
                     <div class="box-body">
-                        <div class="table-responsive">
-                            <table class="table no-margin">
-                                <tbody>
-                                <tr><td>
-                                    操作系统
-                                </td><td></td></tr>
-                                <tr><td>
-                                    内存
-                                </td><td></td></tr>
-                                <tr><td>
-                                    CPU信息
-                                </td><td>
-
-                                </td></tr>
-                                </tbody>
-                            </table>
+                        <span class="sub_title">CPU信息</span>
+                        <div class="sub_content">
+                            <div class="_row">
+                                <span class="sub_label">生产商</span>
+                                <span class="info" v-if="CPU_vendor_info != ''">{{ CPU_vendor_info }}</span>
+                                <span class="gray" v-else>未知</span>
+                            </div>
+                            <div class="_row">
+                                <span class="sub_label">型号</span>
+                                <span class="info" v-if="CPU_model_info != ''">{{ CPU_model_info }}</span>
+                                <span class="gray" v-else>未知</span>
+                            </div>
+                            <div class="_row">
+                                <span class="sub_label">核数</span>
+                                <span class="info" v-if="CPU_cores_info != ''">{{ CPU_cores_info }}</span>
+                                <span class="gray" v-else>未知</span>
+                            </div>
+                            <div class="_row">
+                                <span class="sub_label">主频</span>
+                                <span class="info" v-if="CPU_freq_info != ''">{{ CPU_freq_info }}</span>
+                                <span class="gray" v-else>未知</span>
+                            </div>
+                        </div>
+                        <span class="sub_title">操作系统</span>
+                        <div class="sub_content">
+                            <div class="_row">
+                                <span class="sub_label">类型</span>
+                                <span class="info" v-if="OS_name_info != ''">{{ OS_name_info }}</span>
+                                <span class="gray" v-else>未知</span>
+                            </div>
+                            <div class="_row">
+                                <span class="sub_label">版本</span>
+                                <span class="info" v-if="OS_distro_info != ''">{{ OS_distro_info }}</span>
+                                <span class="gray" v-else>未知</span>
+                            </div>
+                            <div class="_row">
+                                <span class="sub_label">内核版本</span>
+                                <span class="info" v-if="OS_kernel_info != ''">{{ OS_kernel_info }}</span>
+                                <span class="gray" v-else>未知</span>
+                            </div>
+                            <div class="_row">
+                                <span class="sub_label">架构</span>
+                                <span class="info" v-if="OS_arch_info != ''">{{ OS_arch_info }}</span>
+                                <span class="gray" v-else>未知</span>
+                            </div>
+                        </div>
+                        <span class="sub_title">内存</span>
+                        <div class="sub_content">
+                            <div class="_row">
+                                <span class="sub_label">总内存</span>
+                                <span class="info" v-if="RAM_info != ''">{{ RAM_info }}</span>
+                                <span class="gray" v-else>未知</span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -37,9 +74,80 @@
 </template>
 
 <script>
-
+    import WebSocket from "../../lib/websocket.js"
+    export default {
+        data(){
+            return {
+                "CPU_model_info" : "",
+                "CPU_vendor_info" :"",
+                "CPU_cores_info" : "",
+                "CPU_freq_info" : "",
+                "OS_name_info" : "",
+                "OS_arch_info" : "",
+                "OS_distro_info" : "",
+                "OS_kernel_info" : "",
+                "RAM_info" : ""
+            }
+        },
+        computed:{
+            allow_password_amend(){
+                if(this.ori_password.length > 0 && this.new_password.length > 0){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        },
+        methods:{
+            aj_get_server_info(){
+                let ws = new WebSocket();
+                let v = this;
+                ws.ajax("GET", "/super_admin/info/get_server_info", (msg)=>{
+                    v.CPU_model_info = msg.cpu.model;
+                    v.CPU_vendor_info = msg.cpu.vendor;
+                    v.CPU_cores_info = msg.cpu.cores;
+                    v.CPU_freq_info = msg.cpu.freq + " MHz";
+                    v.OS_name_info = msg.OS.name;
+                    v.OS_arch_info = msg.OS.arch;
+                    v.OS_kernel_info = msg.OS.kernel;
+                    v.OS_distro_info = msg.OS.distro;
+                    v.RAM_info = msg.memory + " GiB";
+                },(code)=>{
+                });
+            }
+        },
+        mounted(){
+            this.aj_get_server_info();
+        }
+    }
 </script>
 
-<style>
+<style scoped>
+span.sub_title{
+    display:inline-block;
+    margin-top:0.5rem;
+    font-weight: bold;
+    font-size: 1.7rem;
+    margin-bottom: 0.5rem;
+}
 
+div.sub_content{
+    padding-left: 4rem;
+}
+
+span.gray{
+    color: #777;
+}
+div._row{
+    line-height: 2.2rem;
+}
+
+div._row span.sub_label{
+    margin-right: 1rem;
+    font-size: 1.5rem;
+}
+
+span.info{
+    color: darkblue;
+}
 </style>
