7ade265e7200872ba9610f9ccfab0c54322c790f
DemoHn <demohn2016@gmail.com>
2019-01-29 00:12:28 +0800

    feat: add new_template from dbmigrate

---
+++ pkg/dbmigrate/new_template.go
@@ -0,0 +1,9 @@
+package dbmigrate
+
+const pkgName = "github.com/DemoHn/obsidian-panel/pkg/dbmigrate"
+
+// NewTemplate - write a migration file with current timestamp to destination folder
+func NewTemplate(destFolder string) error {
+	// TODO: add template string
+	return nil
+}
