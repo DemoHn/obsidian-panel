2eb9fa4a58060395f522c0cfd2ba45f865a5089c
DemoHn <demohn2016@gmail.com>
2019-01-30 23:40:15 +0800

    feat: add tmp script to create migration files by dev CLI

--- _dev/cli/main.go
+++ _dev/cli/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/DemoHn/obsidian-panel/pkg/dbmigrate"
+)
 
 func main() {
-	fmt.Println("test main")
+	args := os.Args
+
+	if len(args) > 1 {
+		if args[1] == "migrate:new" {
+			if _, err := dbmigrate.NewTemplate(args[2], "app/migrations"); err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	fmt.Println("CLI execution finish")
 }
