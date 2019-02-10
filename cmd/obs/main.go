fe6a842a6155f71184898d239eecfdd4782ba815
DemoHn <demohn2016@gmail.com>
2019-02-10 15:58:30 +0800

    feat: make `obs` executable!

--- cmd/obs/main.go
+++ cmd/obs/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/DemoHn/obsidian-panel/app"
+	"github.com/spf13/cobra"
+)
+
+// flags
+var configPath string
+
+var rootCmd = &cobra.Command{
+	Use:     "obs",
+	Short:   "obsidian-panel main command",
+	Long:    "obsidian-panel main command",
+	Version: "0.7.0",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return app.Init(configPath, false)
+	},
+	SilenceUsage: true,
+}
 
 func main() {
-	fmt.Println("Test command for obsidian-panel")
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&configPath, "config", "c", "config.yml", "config path")
 }
--- go.mod
+++ go.mod
@@ -15,6 +15,8 @@ require (
 	github.com/lib/pq v1.0.0 // indirect
 	github.com/mattn/go-sqlite3 v1.10.0 // indirect
 	github.com/sirupsen/logrus v1.3.0
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
 	google.golang.org/appengine v1.4.0 // indirect
 	gopkg.in/yaml.v2 v2.2.2
 )
