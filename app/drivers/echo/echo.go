2af43dcca6b09a2411674919783670229967206e
DemoHn <demohn2016@gmail.com>
2019-04-03 00:37:28 +0800

    feat: add api route group to help define routes

--- app/drivers/echo/echo.go
+++ app/drivers/echo/echo.go
@@ -1,6 +1,8 @@
 package echo
 
 import (
+	"fmt"
+
 	"github.com/DemoHn/obsidian-panel/infra"
 	"github.com/labstack/echo"
 )
@@ -14,20 +16,31 @@ type Driver struct {
 // Context - same as `echo.Context`
 type Context = echo.Context
 
+// Group - same as `echo.Group`
+type Group = echo.Group
+
 // New - new echo instance
 func New(config *infra.Config) (*Driver, error) {
 	var address string
 	var err error
 
 	e := echo.New()
-
 	if address, err = config.FindString("api.address"); err != nil {
 		return nil, err
 	}
+	// load middlewares
+	loadMiddlewares(e)
+
 	return &Driver{
 		Echo:    e,
 		address: address,
 	}, nil
+}
+
+// GetAPIRouter - get an router instance prefix with /api/v1.0
+func (drv *Driver) GetAPIRouter(version string) *Group {
+	var prefix = fmt.Sprintf("/api/v%s", version)
+	return drv.Echo.Group(prefix)
 }
 
 // Listen - listen to a preset port
