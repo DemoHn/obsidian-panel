a0621137a1a39f044e4295c285e1da8ce61fb84b
DemoHn <demohn2016@gmail.com>
2019-04-12 00:21:54 +0800

    feat: add permission middleware

--- app/drivers/echo/middlewares.go
+++ app/drivers/echo/middlewares.go
@@ -8,7 +8,34 @@ import (
 	"github.com/go-playground/validator"
 )
 
+// ExtendContext - extends echo.Context with useful functions
+// e.g. hasPermission()
+type ExtendContext struct {
+	echo.Context
+	permissions []string
+}
+
+// SetPermissions - set permissions for this request
+func (ec *ExtendContext) SetPermissions(permissions ...string) {
+	var perms = []string{}
+	perms = append(perms, permissions...)
+	ec.permissions = perms
+}
+
+// GetPermissions - get perms of this request
+func (ec *ExtendContext) GetPermissions() []string {
+	return ec.permissions
+}
+
 func loadMiddlewares(e *echo.Echo) {
+	// load custom extend context
+	e.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &ExtendContext{c, []string{}}
+			return h(cc)
+		}
+	})
+	// logger & error recover
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
